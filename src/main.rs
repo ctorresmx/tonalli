51aa7d01a56dd9053ced80e228f748aa8627970e
Carlos Torres <j.carlos.torres@me.com>
2026-03-06 12:41:47 -0800

    add tui with basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod agents;
+mod tui;
 
 use std::error::Error;
 
@@ -8,20 +9,13 @@ use crate::agents::{Chat, GeminiAgent};
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv::dotenv().ok();
 
-    let api_key = std::env::var("AGENT_API_KEY");
-    let model = std::env::var("AGENT_MODEL");
+    let api_key = std::env::var("AGENT_API_KEY").expect("AGENT_API_KEY not set");
+    let model = std::env::var("AGENT_MODEL").expect("AGENT_MODEL not set");
 
-    let agent = GeminiAgent::new(api_key.unwrap(), model.unwrap());
+    let agent = GeminiAgent::new(api_key, model);
+    let chat = Chat::new(agent);
 
-    let mut chat = Chat::new(agent);
-
-    let response = chat.send("Hi there!").await?;
-
-    println!("history: {:?}", response);
-
-    let response2 = chat.send("Why is the sky blue? Be concise").await?;
-
-    println!("history: {:?}", response2);
+    tui::run(chat).await?;
 
     Ok(())
 }
